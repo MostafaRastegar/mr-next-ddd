0bb2dca9ded0be09ddd3fece3552234e37c73ecf
mostafa rastegar <m.rastegar991@gmail.com>
2023-12-09 18:51:34 +0330

    feat: add login and register methods

---
+++ src/modules/users/presentations/nextSSR/UserNextSSR.ts
@@ -0,0 +1,38 @@
+import UserController from "../controllers/UserController";
+import UserService from "@/modules/users/applications/services/UserService";
+import { UserRepository } from "@/modules/users/infrastructure";
+import {
+  UserLoginParams,
+  UserRegisterParams,
+} from "@/modules/users/domains/models/User";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const userService = UserService(UserRepository, cookies, redirect);
+const userController = UserController(userService);
+
+function UserNextSSR() {
+  return {
+    userLogin: async (formData: FormData) => {
+      const rawFormData: UserLoginParams = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+      };
+      const result = await userController.userLogin(rawFormData);
+
+      return result;
+    },
+    userRegister: async (formData: FormData) => {
+      const rawFormData: UserRegisterParams = {
+        email: formData.get("email") as string,
+        username: formData.get("username") as string,
+        password: formData.get("password") as string,
+      };
+      const result = await userController.userRegister(rawFormData);
+
+      return result;
+    },
+  };
+}
+
+export default UserNextSSR;
